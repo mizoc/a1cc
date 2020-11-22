beb2ef0cf7b60094de889741fee66f0c4a3a36b3
Mizoc <yaesuft729@gmail.com>
2020-11-22 23:14:27 +0900

    :books: :recycle: Add README, LICENSE Changed indent 4 space => 2space

---
+++ README.md
@@ -0,0 +1,6 @@
+# a1cc
+
+```bash
+$ chmod +x a1cc.sh
+$ ./a1cc.sh hamlog_data.csv a1c_member_list
+```
--- a1cc.sh
+++ a1cc.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
 test $# -ne 2 && {
-	echo "[Usage]   $0 hamlog_data.csv a1c_member_list"
-	exit 1
+  echo "[Usage]   $0 hamlog_data.csv a1c_member_list"
+  exit 1
 }
 TEMP=mktemp
-awk -F ',' '{print $1}' $1 |sed 's%/.*%%g'|sort|uniq >$TEMP
-M=`comm -12 $TEMP <(awk '{print $1}' $2|sort)|wc -l`
+awk -F ',' '{print $1}' $1 | sed 's%/.*%%g' | sort | uniq >$TEMP
+M=$(comm -12 $TEMP <(awk '{print $1}' $2 | sort) | wc -l)
 echo "members: $M"
-A=`wc -l <$TEMP`
+A=$(wc -l <$TEMP)
 echo "all: $A"
 rm -rf $TEMP
 
-if test $A -le $(($M*5));then
-	echo OK
+if test $A -le $((M * 5)); then
+  echo OK
 else
-	echo NG
+  echo NG
 fi
